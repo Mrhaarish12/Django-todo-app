ae6439c5bf9eb3b82c3ecd66b10f73cce6b95e44
Haarish Khan <khan1281979harry@gmail.com>
2023-11-21 01:39:56 +0530

    First Commit 11min Pause

---
+++ todoapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def task_list(request):
+    context = {}
+    return render(request, "task_list.html", context)
+
+def task_update(request):
+    context = {}
+    return render(request, "task_list.html", context)
+
+def task_delete(request):
+    context = {}
+    return render(request, "task_delete.html", context)
