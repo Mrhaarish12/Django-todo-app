1640b1e16aec855ca8aa21b0a8eec0af32a56f4b
Haarish Khan <khan1281979harry@gmail.com>
2023-11-25 17:22:05 +0530

    Day 3 Title in Forms added removing it from html

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 # Create your views here.
 def task_list(request):
-    context = {}
+    tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect("task-list")
+    context = {"tasks": tasks, "form": form}
     return render(request, "task_list.html", context)
 
 def task_update(request):
